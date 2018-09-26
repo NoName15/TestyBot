07bde6ea8e3e1f6ae09123abad1edcedc7e120eb
NoName15 <43522880+NoName15@users.noreply.github.com>
2018-09-26 21:44:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,13 @@
 // Load up the discord.js library
 const Discord = require("discord.js");
-
-// This is your client. Some people call it `bot`, some people call it `self`, 
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
+const bot = new Discord.Client({disableEveryone: true});
 const client = new Discord.Client();
-
-// Here we load the config.json file that contains our token and our prefix values. 
 const config = require("./config.json");
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
 
 client.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
+
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
+
   client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
@@ -33,10 +25,7 @@ client.on("guildDelete", guild => {
 
 
 client.on("message", async message => {
-  // This event will run on every single message received, from any channel or DM.
-  
-  // It's good practice to ignore other bots. This also makes your bot ignore itself
-  // and not get into a spam loop (we call that "botception").
+ 
   if(message.author.bot) return;
   
   // Also good practice to ignore any message that does not start with our prefix, 
