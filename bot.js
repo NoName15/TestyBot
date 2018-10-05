4485c51bd8dffd6effbc9b2ffec7116c46109f6f
NoName15 <43522880+NoName15@users.noreply.github.com>
2018-10-05 23:07:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = "?"
-
+const ms = require("ms");
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   console.log('')
@@ -83,6 +83,37 @@ client.on('message',message =>{
    
     }
   });
+
+
+if (command === "remind") {
+
+
+    let reminderTime = args[0];
+    if (!reminderTime) return message.channel.send("**Specify a time for me to remind you. Usage: /remind 15min any text or code**");
+
+    let reminder = args.slice(1).join(" ");
+
+    let remindEmbed = new Discord.RichEmbed()
+        .setColor('#ffffff')
+        .setAuthor(`${message.author.username}`, message.author.displayAvatarURL)
+        .addField("Reminder", `\`\`\`${reminder}\`\`\``)
+        .addField("Time", `\`\`\`${reminderTime}\`\`\``)
+        .setTimestamp();
+
+    message.channel.send(remindEmbed);
+
+
+    setTimeout(function() {
+        let remindEmbed = new Discord.RichEmbed()
+            .setColor('#ffffff')
+            .setAuthor(`${message.author.username}`, message.author.displayAvatarURL)
+            .addField("Reminder", `\`\`\`${reminder}\`\`\``)
+            .setTimestamp()
+
+        message.channel.send(remindEmbed);
+    }, ms(reminderTime));
+
+}
 client.on('message', msg => {
   if (msg.content === 'السلام عليكم') {      
     msg.channel.send("وعليكـم الســلام ورحمة الله وبركاتـه")
