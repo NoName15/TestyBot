ebaed83dd83fe4529d21e2537688b5b48ab336ac
NoName15 <43522880+NoName15@users.noreply.github.com>
2018-10-12 16:09:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,25 @@ client.on('ready', () => {
   console.log('')
   console.log('')
 });
+ var prefix = "?";
+var cats = [
 
+"https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
+"https://www.petfinder.com/wp-content/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg",
+"http://www.i-love-cats.com/images/2015/04/12/cat-wallpaper-38.jpg",
+"https://www.aspca.org/sites/default/files/cat-care_urine-marking_main-image.jpg",
+"https://vignette1.wikia.nocookie.net/houseofnight/images/8/8b/Cats.jpg/revision/latest?cb=20130812053537",
+"https://s-media-cache-ak0.pinimg.com/originals/f0/3b/76/f03b7614dfadbbe4c2e8f88b69d12e04.jpg",
+"http://www.rd.com/wp-content/uploads/sites/2/2016/04/15-cat-wants-to-tell-you-attention.jpg"
+]
+    client.on('message', message => {
+        var args = message.content.split(" ").slice(1);
+    if(message.content.startsWith(prefix + 'cat')) {
+         var cat = new Discord.RichEmbed()
+.setImage(cats[Math.floor(Math.random() * cats.length)])
+message.channel.sendEmbed(cat);
+    }
+});
 //code
 const yourID = "480407581085532180"; //Instructions on how to get this: https://redd.it/40zgse // AlphaCodes
 const setupCMD = "?createrolemessage"
@@ -479,6 +497,7 @@ client.on('message',async message => {
 });
 client.on('message', message => {
 if (message.content.startsWith(prefix + 'gameshelp')) { //ALPHACODES
+	message.channel.send('Check you dms')  
     let pages = [`
 ***__وصف عن البوت__***
 **
@@ -564,6 +583,7 @@ if (message.content.startsWith(prefix + 'help')) { //ALPHACODES
 『?gameshelp / مساعدة حول الالعاب 』
 『?topinv / معرفة اكثر واحد جاب ناس 』
 『?remind / جعل البوت يذكرك بشيء 』
+『?cat / يظهر لك صورة قط عشوائية 』
 **
    
 `]
